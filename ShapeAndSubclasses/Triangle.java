15277ebdf3126f23660d99bc8e50c35edbcea68a
Zeusiek <mateo.zeusiek.targonski@gmail.com>
2021-02-09 13:02:51 +0100

    Add files via upload

---
+++ ShapeAndSubclasses/Triangle.java
@@ -0,0 +1,15 @@
+package Targoński_3P_Praktyka.ShapeAndSubclasses;
+
+public class Triangle extends Shape{
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+
+}
